f565b6b9dce2252a5936ae0c82d0746cbd133482
AdityaGaur7 <adgaur027@gmail.com>
2023-07-17 02:55:00 +0530

    new files

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function f1(){
+
+    var ans = document.querySelector('#search').value;
+    var bg = document.querySelector('#photo');
+    var add = "http://source.unsplash.com/1980x1080/?"+ ans;
+    img.src=add;
+
+}
+
+// Hello there ! , hope you are doing well , If you are reading this , then you are the person I  might be looking for
+// I know that you have been looking upto to my every code ever since I started , 
+// I have written this intentionally to let you know that I have something for you 
+// I have a surprise for you , Even if i know you or not , you are special to me because every existence matters -- just leave this line 😅
+
+
+// here is a code  👉 LuvYQ1kuozsprtadgaur
+
+// And the link 👉  redeem.adgaur027.repl.co
+
+// just you copy and paste url and got the surprise ⭐
